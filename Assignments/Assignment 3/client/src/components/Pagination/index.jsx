52f80331fbbe1d65499912ac214368cc9baeffc6
kerrydachow <kerry_chow@hotmail.ca>
2022-11-23 23:49:14 -0800

    A3 - Add pagination accent color

--- Assignments/Assignment 3/client/src/components/Pagination/index.jsx
+++ Assignments/Assignment 3/client/src/components/Pagination/index.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Pagination, Container } from "@mui/material";
 
 const PokemonPagination = ({ currentPage, setCurrentPage, totalPages }) => {
+
   return (
-    <Container sx={{ display: "flex", justifyContent: "center" }}>
+    <Container sx={{ display: "flex", justifyContent: "center", mb: 4 }}>
       <Pagination
         variant="outlined"
         shape="rounded"
         count={totalPages}
-        size="large"
+        size="medium"
         page={currentPage}
+        color="secondary"
         onChange={(event, page) => setCurrentPage(page)}
       />
     </Container>
