5af14a4e32cea1a7710fbf173b578a449150d36a
kerrydachow <kerry_chow@hotmail.ca>
2022-11-23 17:34:27 -0800

    A3 - Create MUI theme

--- Assignments/Assignment 3/client/src/App.js
+++ Assignments/Assignment 3/client/src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 import { Routes, Route } from "react-router-dom";
 import { ROUTES } from "./lib/constants";
 import Header from "./components/ui/Header";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path={ROUTES.LANDING} element={<LandingPage />} exact />
-      </Routes>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="content">
+            <Header />
+            <Routes>
+              <Route path={ROUTES.LANDING} element={<LandingPage />} exact />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
